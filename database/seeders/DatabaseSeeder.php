98736847ff681643d234c004d940bd402382adf7
ThaksinhCW3 <thaksin.mnv785@gmail.com>
2025-08-19 23:02:54 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // seed default roles/users
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Staff User',
+            'email' => 'staff@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'staff',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Borrower User',
+            'email' => 'borrower@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'borrower',
+        ]);
+    }
+}
