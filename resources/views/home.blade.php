afce2767cba8fbecb16f82dc2c53a3bf9c2fec4c
unikZer0 <unik09john@gmail.com>
2025-08-20 22:45:28 +0700

    test card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,22 @@
 <x-app-layout>
     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-2xl font-bold mb-4">tesss</h1>
-        @auth
-            <div>Your role: <span class="font-semibold">{{ Auth::user()->role }}</span></div>
-        @endauth
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-7xl mx-auto">
+            <div class="bg-white rounded-2xl shadow-lg hover:shadow-2xl transition overflow-hidden group">
+                <div class="relative">
+                    <img src="https://images.unsplash.com/photo-1709258228137-19a8c193be39?q=80&w=2011&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        alt="Product Image" class="w-full h-60 object-cover group-hover:scale-105 transition-transform" />
+                    <span class="absolute top-3 right-3 bg-red-600 text-white text-xs px-2 py-1 rounded">avalaible</span>
+                </div>
+                <div class="p-5">
+                    <h3 class="text-lg font-bold text-gray-900 mb-1">Running Shoes Pro</h3>
+                    <p class="text-sm text-gray-500 mb-2">Lightweight · Breathable</p>
+                    <div class="flex justify-between items-center mt-4">
+                        <span class="text-lg font-semibold text-green-600">$89.99</span>
+                        <button class="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800">rent</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </x-app-layout>
