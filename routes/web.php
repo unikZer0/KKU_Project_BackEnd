3f2f990d4eda381002b3fc28aa84811604b1d13e
unikZer0 <unik09john@gmail.com>
2025-08-21 13:49:33 +0700

    delete profile

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,8 @@ Route::get('/', function () {
 // })->middleware(['auth', 'verified'])->name('/');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
+    //api for auth
+    //here
     // Role-based areas
     Route::middleware('can:admin')->group(function () {
         Route::get('/admin', function () {
