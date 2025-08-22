b60d0c550d916f9be27be4cc77397b9af7cd6d1e
unikZer0 <unik09john@gmail.com>
2025-08-22 11:20:54 +0700

    filter n pagi

--- app/Http/Controllers/Borrowers/BorrowersHomeController.php
+++ app/Http/Controllers/Borrowers/BorrowersHomeController.php
@@ -7,5 +7,21 @@ use Illuminate\Http\Request;
 
 class BorrowersHomeController extends Controller
 {
-    
+//   public function index(Request $request)
+// {
+//     $query = Equipment::query();
+
+//     if ($request->status) {
+//         $query->where('status', $request->status);
+//     }
+
+//     if ($request->q) {
+//         $query->where('name', 'like', "%{$request->q}%");
+//     }
+
+//     $equipments = $query->paginate(10);
+
+//     return response()->json($equipments);
+// }
+  
 }
---
+++ resources/views/components/pagination.blade.php
@@ -0,0 +1,5 @@
+<div class="">
+    <div id="pagination">
+        
+    </div>
+</div>
