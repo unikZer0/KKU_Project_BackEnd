f0eda49a889f39f1a8bbb714e326c36046921f3c
ThaksinhCW3 <thaksin.mnv785@gmail.com>
2025-08-22 13:59:00 +0700

    admin route

--- app/Http/Controllers/Borrowers/BorrowersHomeController.php
+++ app/Http/Controllers/Borrowers/BorrowersHomeController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Borrowers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Categories;
 use Illuminate\Http\Request;
+
+use App\Models\Equipments;
 
 class BorrowersHomeController extends Controller
 {
-    
+    public function home()
+    {
+        $equipments = Equipments::all();
+        $categories = Categories::all();
+        return view('home', compact('equipments', 'categories'));
+    }
 }
