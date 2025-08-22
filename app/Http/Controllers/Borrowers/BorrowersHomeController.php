8d8e03ed4a5af97573d7e63e100f1099f4db8fe7
ThaksinhCW3 <thaksin.mnv785@gmail.com>
2025-08-22 14:03:33 +0700

    admin route

--- app/Http/Controllers/Borrowers/BorrowersHomeController.php
+++ app/Http/Controllers/Borrowers/BorrowersHomeController.php
@@ -11,21 +11,20 @@ use App\Models\Category;
 
 class BorrowersHomeController extends Controller
 {
-//   public function index(Request $request)
-// {
-//     $query = Equipment::query();
+    public function index(Request $request)
+    {
+        $query = Equipment::query();
 
-//     if ($request->status) {
-//         $query->where('status', $request->status);
-//     }
+        if ($request->status) {
+            $query->where('status', $request->status);
+        }
 
-//     if ($request->q) {
-//         $query->where('name', 'like', "%{$request->q}%");
-//     }
+        if ($request->q) {
+            $query->where('name', 'like', "%{$request->q}%");
+        }
 
-//     $equipments = $query->paginate(10);
+        $equipments = $query->paginate(10);
 
-//     return response()->json($equipments);
-// }
-  
+        return response()->json($equipments);
+    }
 }
