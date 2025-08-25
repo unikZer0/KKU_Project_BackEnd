b3ca16750fb1a9e6bb4833a326917745d2ffb89b
unikZer0 <unik09john@gmail.com>
2025-08-25 16:09:36 +0700

    filter and cate

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,19 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Str;
 class Category extends Model
 {
     protected $table = 'categories';
     protected $fillable = [
         "id",
         "name",
-    ];
 
-    // Relationship with Equipment
-    public function equipments()
+        "cate_id"
+    ];
+        protected static function generatecate_id()
     {
-        return $this->hasMany(\App\Models\Equipment::class, 'categories_id'); 
-        // 'categories_id' is the foreign key in the equipments table
+        do {
+            $cate_id = strtoupper(Str::random(10));
+        } while (self::where('cate_id', $cate_id)->exists());
+
+        return $cate_id;
+    }
+
+protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->cate_id = self::generateEcate_id();
+        });
     }
 }
