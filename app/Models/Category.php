6a0c227cf6c9192f3247200af94415de1423d5c0
phanousith <153116284+phanousith@users.noreply.github.com>
2025-08-26 10:53:39 +0700

    fix Category Controller

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,34 +1,19 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
-use Illuminate\Support\Str;
+
 class Category extends Model
 {
     protected $table = 'categories';
     protected $fillable = [
         "id",
         "name",
-
-        "cate_id"
     ];
-        protected static function generatecate_id()
+
+    // Add this relationship
+    public function equipments()
     {
-        do {
-            $cate_id = strtoupper(Str::random(10));
-        } while (self::where('cate_id', $cate_id)->exists());
-
-        return $cate_id;
-    }
-
-protected static function boot()
-    {
-        parent::boot();
-
-        static::creating(function ($model) {
-            $model->cate_id = self::generateEcate_id();
-        });
+        return $this->hasMany(Equipment::class, 'categories_id');
     }
 }
